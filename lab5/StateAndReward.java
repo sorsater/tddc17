32860ff2dcfb900afd4efe2db1ccd2b336309d17
michael <michael.sorsater@gmail.com>
2016-10-04 14:38:00 +0200

    Commented version of the file

--- lab5/StateAndReward.java
+++ lab5/StateAndReward.java
@@ -1,19 +1,32 @@
 public class StateAndReward {
 
+	/*
+		* the main idea for the hover function is:
+		* give reward for angle/vy in two steps. (ignore vx)
+		* first, "raw" reward which quickly direct the rocket in a close to hover state
+		* second, give "fine tuning" reward in a small area to train the rocket to perform well
+		* To trim the rocket, there exist three really good states which have a high reward.
+		* In those three steps all parameters needs to be really small
+	*/
+
 	// angle
+	// raw
 	public static int ANGLE_REWARD = 10;
 	public static int ANGLE_PRECISION = 6;
 	public static double ANGLE_LIMIT = Math.PI;
 
+	// fine tuning
 	public static int ANGLE_TUNING_REWARD = 15;
 	public static int ANGLE_TUNING_PRECISION = 6;
 	public static double ANGLE_TUNING_LIMIT = 0.3; //ANGLE_LIMIT / 10;
 
 	// velocity
+	// raw
 	public static int VY_REWARD = 8;
 	public static int VY_PRECISION = 6;
 	public static double VY_LIMIT = 8;
 
+	// fine tuning
 	public static int VY_TUNING_REWARD = 12; // 6
 	public static int VY_TUNING_PRECISION = 6;
 	public static double VY_TUNING_LIMIT = 2; //VY_LIMIT / 5;
@@ -32,12 +45,10 @@ public class StateAndReward {
 	public static double VX_2 = 0.1;
 	public static double VX_3 = 0.3;
 
+	// reward for the three states
 	public static int REWARD_1 = 5000;
 	public static int REWARD_2 = 2000;
 	public static int REWARD_3 = 500;
-
-
-
 
 	public static String getStateAngle(double angle, double vx, double vy) {
 
@@ -124,11 +135,11 @@ public class StateAndReward {
 			state += "GODLIKE";
 		}
 		else if(angle_abs < ANGLE_2 && vy_abs < VY_2 && vx_abs < VX_2){
-			state += "godlike";
+			state += "GREAT";
 		}
 
 		if(angle_abs < ANGLE_3 && vy_abs < VY_3 && vx_abs < VX_3){
-			state += "insane";
+			state += "GOOD";
 		}
 
 		return state;
